c0ec337bb58bce545a4aa08aad00ac3502734128
Vlnts <valentsionovaviktoria@gmail.com>
2022-06-25 02:40:11 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require("./routes/Auth");
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use("/api/auth", authRouter)
+
+const start = async() => {
+    try {
+        mongoose.connect(process.env.DB_URL)
+        app.listen(PORT, () => {
+            console.log(`Server started on port`, PORT)
+        })
+    } catch (error) {
+        
+    }
+}
+
+start()
